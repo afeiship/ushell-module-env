23cff4cf43d03c5f3dd26663fe40f7706b1edd6b
feizheng <1290657123@qq.com>
2016-11-03 18:33:37 +0800

    add env-init

--- index.sh
+++ index.sh
@@ -2,7 +2,7 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-
 alias e-env='vim /etc/environment';
+alias env-init='sudo sh -c "echo -e \"LANG=en_US.utf-8\nLC_ALL=en_US.utf-8\" >> /etc/environment"';
 
 unset ROOT_PATH;
